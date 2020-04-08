03da10421e943a6d92b5dc86de23b48831f710b5
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-08 15:45:49 +0100

    Used the sequelize schema

--- data/pizzas.js
+++ data/pizzas.js
@@ -1,38 +1,36 @@
-const { filter, orderBy, values } = require('lodash')
+const { filter, orderBy, values } = require('lodash');
 
-const Pizza = require('../models/pizza')
-const ImageStore = require('../lib/imageStore')
+const Pizza = require('../models/pizza');
+const ImageStore = require('../lib/imageStore');
+const PizzaStore = require('./pizzaStore');
 
-const pizzas = {}
+const pizzas = {};
 
-async function create (name, toppings, img, username) {
-  const imgUrl = await ImageStore.save(name.replace(/ /g, '-'), img)
-  const pizza = new Pizza(name, toppings, imgUrl, username)
-  pizzas[pizza.id] = pizza
-  return pizza
+async function create(name, toppings, img, username) {
+  const imgUrl = await ImageStore.save(name.replace(/ /g, '-'), img);
+  const pizza = new Pizza(name, toppings, imgUrl, username);
+  return PIzzaStore.create(prefPizza(pizza));
 }
 
 // for mocks that don't need pizza images saved
-function batchImport (name, toppings, imgUrl, username) {
-  const pizza = new Pizza(name, toppings, imgUrl, username)
-  pizzas[pizza.id] = pizza
+function batchImport(name, toppings, imgUrl, username) {
+  const pizza = new Pizza(name, toppings, imgUrl, username);
+  pizzas[pizza.id] = pizza;
 }
 
-async function getForUser (username) {
-  const userPizzas = filter(pizzas, pizza =>
-    pizza.username === username
-  )
-  return userPizzas
+async function getForUser(username) {
+  const userPizzas = filter(pizzas, (pizza) => pizza.username === username);
+  return userPizzas;
 }
 
-async function getRecent () {
-  const recentPizzas = orderBy(pizzas, ['created'], ['desc'])
-  return values(recentPizzas).splice(0, 5)
+async function getRecent() {
+  const recentPizzas = orderBy(pizzas, ['created'], ['desc']);
+  return values(recentPizzas).splice(0, 5);
 }
 
-async function get (pizzaId) {
-  if (!pizzas[pizzaId]) throw new Error('Pizza not found')
-  return pizzas[pizzaId]
+async function get(pizzaId) {
+  if (!pizzas[pizzaId]) throw new Error('Pizza not found');
+  return pizzas[pizzaId];
 }
 
 module.exports = {
@@ -40,5 +38,5 @@ module.exports = {
   create,
   get,
   getForUser,
-  getRecent
-}
+  getRecent,
+};
