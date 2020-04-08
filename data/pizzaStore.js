a03327a4e2d936d4bd74bf3b0b1259c7686a1c93
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-08 15:38:30 +0100

    Created and synced a pizza object

--- data/pizzaStore.js
+++ data/pizzaStore.js
@@ -9,3 +9,28 @@ const pgClient = new Sequelize(database, username, password, {
   host,
   dialect: 'postgres',
 });
+
+const Pizza = pgClient.define('pizza', {
+  id: {
+    type: Sequelize.STRING,
+    primaryKey: true,
+  },
+  name: {
+    type: Sequelize.STRING,
+  },
+  toppings: {
+    type: Sequelize.STRING,
+  },
+  username: {
+    type: Sequelize.STRING,
+  },
+  created: {
+    type: Sequelize.BIGINT,
+  },
+});
+
+Pizza.sync().then(() => {
+  console.log('Postgres connection ready');
+});
+
+module.exports = Pizza;
