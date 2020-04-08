4ba2d3e635e20a5e97afbc10d181d7843aa6e5f9
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-04-08 16:20:40 +0100

    Put item to a dynamo table

---
+++ data/dynamoStore.js
@@ -0,0 +1,22 @@
+const AWS = require('aws-sdk');
+
+AWS.config.update({ region: 'eu-west-3' });
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function putItem(table, item) {
+  return new Promise((resolve, reject) => {
+    const params = {
+      TableName: table,
+      Item: item,
+    };
+
+    dynamodb.put(params, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+}
